e25e5888d4b34bacb149742b65b04d1a1c007d10
Papasha883 <kova.pavel.a@gmail.com>
2026-03-06 19:13:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
+import os
 import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-TELEGRAM_TOKEN = "8688216650:AAEC7vFagTxI7RKojYeIILmFh4jcpDa5yXE"
-OPENROUTER_API_KEY = "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"
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+OPENROUTER_API_KEY = os.environ.get('OPENROUTER_API_KEY')
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     user_message = update.message.text
 
     headers = {
@@ -34,17 +34,12 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(answer)
 
-
 def main():
-
     print("Бот запущен...")
-
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
+
